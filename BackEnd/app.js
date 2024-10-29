81d7aca7a349af6e246b2f8bcb4eae57ec601075
Guerwan Lescrohart <glescrohart@gmail.com>
2024-10-29 17:50:58 +0100

    feat: API + auth

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -1,6 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+const stuffRoutes = require('./routes/stuff');
+const userRoutes = require('./routes/user');
 
 const app = express();
+
+mongoose.connect('mongodb+srv://admin:JFm5ETNJUdFLpekm@monvieuxgrimoire.esp8e.mongodb.net/?retryWrites=true&w=majority&appName=MonVieuxGrimoire',)
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 app.use(express.json());
 
@@ -11,34 +19,9 @@ app.use((req, res, next) => {
     next();
 });
 
-app.post('/api/stuff', (req, res, next) => {
-    console.log(req.body);
-    res.status(201).json({
-    message: 'Objet créé !'
-    });
-});
+app.use('/api/stuff', stuffRoutes);
 
-app.use('/api/stuff', (req, res, next) => {
-    const stuff = [
-        {
-            _id: 'oeihfzeoi',
-            title: 'Mon premier objet',
-            description: 'Les infos de mon premier objet',
-            imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-            price: 4900,
-            userId: 'qsomihvqios',
-        },
-        {
-            _id: 'oeihfzeomoihi',
-            title: 'Mon deuxième objet',
-            description: 'Les infos de mon deuxième objet',
-            imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-            price: 2900,
-            userId: 'qsomihvqios',
-        },
-        ];
-        res.status(200).json(stuff);
-});
-
+app.use('/api/stuff', stuffRoutes);
+app.use('/api/auth', userRoutes);
 
 module.exports = app;
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # OpenClassrooms-Projet6
  Développez le back-end d'un site de notation de livres
+
+ Database user's credentials : 
+ Username : admin
+ Password : JFm5ETNJUdFLpekm
+
